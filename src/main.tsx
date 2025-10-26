7869d4091574bb7bb68529822e0c444ee15f3c99
DanSnow <dododavid006@gmail.com>
2025-10-26 11:31:05 +0800

    fix: use hash history

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./tailwind.css";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
+import {
+  createHashHistory,
+  createRouter,
+  RouterProvider,
+} from "@tanstack/react-router";
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
 const router = createRouter({
   routeTree,
+  history: createHashHistory(),
   basepath: import.meta.env.BASE_URL,
 });
 
